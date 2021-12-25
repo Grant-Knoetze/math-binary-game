11bec4706ab1a32605533c6f8945b52495e3a34e
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2021-12-25 18:05:19 +0300

    Change multiple lines in all functions, change comments to docstrings in functions

--- gametasks.py
+++ gametasks.py
@@ -2,32 +2,57 @@
 # These functions do not require any information about an instance or class
 # and are thus not written as part of a class
 
-from os import remove, rename
-
-
 def printInstructions(instruction):
-    return str(instruction)
+    print(instruction)
 
 
-try:
-    def getUserScore(userName):
-        with open("UserScores.txt", "r") as file:
-            for line in userName:
-                line.split()
-                if userName == userName:
-                    return line
-                    file.close()
-                else:
-                    return "-1"
-                file.close()
-                print(line)
+def getUserScore(userName):
+    """This function accepts the user name as a parameter, opens a file
+       containing user scores, reads the file using a for loop, and returns
+       closes the file, and returns the user's score beside the username.
+       The function is nested within a try/except block to handle an IOError
+       if one occurs."""
+    try:
+        input = open('userScores.txt', 'r')
+        for line in input:
+            content = line.split(', ')
+            if content[0] == userName:
+                input.close()
+                return content[1]
+        input.close()
+        return '-1'
+    except IOError:
+        print("File not found. A new file will be created.")
+        input = open('userScores.txt', 'w')
+        input.close()
+        return '-1'
 
-except IOError:
-    print("File not found")
-    open("UserScores.txt", "w")
-    print("-1")
-    "UserScores.txt".close()
 
+def updateUserScore(newUser, userName, score):
+    """" This function relies on the remove and rename functions from the os module
+           to remove the file and rename the file to UserScores.txt.
+           If newUser is True, UserScores.txt is opened in append mode and the user's
+           username and score are written to UserScores.txt using the write function.
+           If newUser is False, the user's username and score are written to the file,
+           we create a temporary file for this purpose"""
+    from os import remove, rename
 
+    if newUser == True:
+        input = open('userScores.txt', 'a')
+        input.write(userName + ', ' + score + '\n')
+        input.close()
+    else:
+        temp = open('userScores.tmp', 'w')
+        input = open('userScores.txt', 'r')
+        for line in input:
+            content = line.split(', ')
+            if content[0] == userName:
+                temp.write(userName + ', ' + score + '\n')
+            else:
+                temp.write(line)
 
+        input.close()
+        temp.close()
 
+        remove('userScores.txt')
+        rename('userScores.tmp', 'userScores.txt')
